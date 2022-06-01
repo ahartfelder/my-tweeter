d1894a5594e0e452893467484b5ecc9a65670791
ahartfelder <ahartfelder@gmail.com>
2022-06-01 09:18:10 -0300

    folders and files created

---
+++ src/ui/components/data-display/Tweet/Tweet.jsx
@@ -0,0 +1,9 @@
+import styles from '.Tweet.module.css';
+
+export default function Tweet() {
+    return (
+        <div className={styles['tweet']}>
+            Tweet
+        </div>
+    );
+}
---
+++ src/ui/pages/index.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/index.module.css';
+
+export default function Index() {
+    return (
+        <div className={styles['index-container']}>
+            
+        </div>
+    ); 
+}
